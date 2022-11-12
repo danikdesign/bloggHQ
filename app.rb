9aa78c7b6db34422fe3797712135c3b149669e8b
danikdesign <danik.newmail@gmail.com>
2022-11-12 17:00:21 +0200

    Added database file. Created Post & Comment models

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
 
-set :database, {adapter: "sqlite3", database: "barbershop.db"}
+set :database, {adapter: "sqlite3", database: "blogg.db"}
+
+class Post < ActiveRecord::Base
+end
+
+class Comment < ActiveRecord::Base
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
