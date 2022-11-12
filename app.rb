c5589a8d827f999402e0a820cf9afad4c0fa5a61
danikdesign <danik.newmail@gmail.com>
2022-11-12 17:42:17 +0200

    Create latest posts list. Update index.erb

--- app.rb
+++ app.rb
@@ -11,6 +11,17 @@ end
 class Comment < ActiveRecord::Base
 end
 
+before do
+  @results = Post.order 'created_at DESC'
+end
+
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+  erb :index
+end
+
+get '/posts' do
+  erb :index
+end
+get '/new' do
+  erb :index
 end
