0f03e341db2194a792fc7f0cafa9cb94c645046d
danikdesign <danik.newmail@gmail.com>
2022-11-12 16:58:17 +0200

    Create Rakefile. Update Gemfile, app.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
